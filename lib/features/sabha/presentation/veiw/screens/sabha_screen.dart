954d3c60837ba9fcd0a0fa0aeb7eb21f152c32c9
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-01-14 02:31:01 +0200

    sabha

--- lib/features/sabha/presentation/veiw/screens/sabha_screen.dart
+++ lib/features/sabha/presentation/veiw/screens/sabha_screen.dart
@@ -1,6 +1,6 @@
 import 'package:azkar/core/local_storage/shared_preferences_manager.dart';
 import 'package:azkar/core/utils/colors.dart';
-import 'package:azkar/features/sabha/presentation/veiw/widgets/appbar_sabha.dart';
+import 'package:azkar/core/utils/strings.dart';
 import 'package:azkar/features/sabha/presentation/veiw/widgets/sabha_screen_body_circle.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -15,6 +15,17 @@ class SabhaScreen extends StatefulWidget {
 
 class _SabhaScreenState extends State<SabhaScreen> {
   int counter = SharedPreferencesManager.getData(key: 'counter') ?? 0;
+  String selectedSabha = "سبحان الله";
+  final List<String> sabhaTypes = [
+    "سبحان الله",
+    "الحمد لله",
+    "الله أكبر",
+    "لا إله إلا الله",
+    "سبحان الله وبحمده",
+    "سبحان الله العظيم",
+    "أستغفر الله",
+    "اللهم صل وسلم على نبينا محمد",
+  ];
 
   void incrementCounter() {
     setState(() {
@@ -33,32 +44,102 @@ class _SabhaScreenState extends State<SabhaScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const AppBarSabha(),
+      appBar: appBar(),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            SizedBox(height: 150.h),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20.h),
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 22.w, vertical: 10.h),
+                decoration: BoxDecoration(
+                  color: ColorsAppLight.primaryColor.withAlpha(40),
+                  borderRadius: BorderRadius.circular(10.r),
+                  boxShadow: [
+                    BoxShadow(
+                      color: ColorsAppLight.primaryColor,
+                      blurRadius: 3,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: DropdownButton<String>(
+                  value: selectedSabha,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedSabha = newValue!;
+                      clearCounter();
+                    });
+                  },
+                  items:
+                      sabhaTypes.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: TextStyle(
+                          fontSize: 20.sp,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                  icon: Icon(
+                    Icons.arrow_drop_down,
+                    color: Colors.white,
+                    size: 33.sp,
+                  ),
+                  dropdownColor: ColorsAppLight.primaryColor,
+                  elevation: 2,
+                ),
+              ),
+            ),
             GestureDetector(
               onTap: () => incrementCounter(),
-              child: SabhaScreenBodyCircle(counter: counter),
-            ),
-            SizedBox(height: 70.h),
-            IconButton(
-              onPressed: () => clearCounter(),
-              icon: CircleAvatar(
-                backgroundColor: const Color.fromARGB(255, 216, 216, 216),
-                radius: 25.w,
-                child: Icon(
-                  Icons.replay,
-                  size: 35.sp,
-                  color: ColorsAppLight.primaryColor,
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 50.h),
+                child: SabhaScreenBodyCircle(
+                  counter: counter,
+                  selectedSabha: selectedSabha,
                 ),
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        StringsAppAR.sabha,
+        style: TextStyle(
+          fontSize: 36.sp,
+          fontWeight: FontWeight.w600,
+          color: Colors.white,
+        ),
+      ),
+      centerTitle: true,
+      flexibleSpace: Container(),
+      elevation: 4,
+      toolbarHeight: 60.h,
+      backgroundColor: ColorsAppLight.primaryColor,
+      actions: [
+        counter != 0
+            ? IconButton(
+                onPressed: () => clearCounter(),
+                icon: Icon(
+                  Icons.refresh,
+                  color: Colors.white,
+                  size: 30.sp,
+                ),
+                tooltip: 'اعادة تعيين العداد',
+              )
+            : SizedBox()
+      ],
     );
   }
 }
--- lib/features/sabha/presentation/veiw/widgets/sabha_screen_body_circle.dart
+++ lib/features/sabha/presentation/veiw/widgets/sabha_screen_body_circle.dart
@@ -6,53 +6,65 @@ class SabhaScreenBodyCircle extends StatelessWidget {
   const SabhaScreenBodyCircle({
     super.key,
     required this.counter,
+    required this.selectedSabha,
   });
 
+  final String selectedSabha;
   final int counter;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200.h,
-      width: 200.w,
+      height: 300.h,
+      width: 300.w,
       decoration: BoxDecoration(
-        shape: BoxShape.circle,
+        color: ColorsAppLight.primaryColor,
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color: ColorsAppLight.primaryColor,
-            blurRadius: 25.sp,
-            spreadRadius: 15.sp,
+            color: const Color.fromARGB(141, 98, 131, 8),
+            blurRadius: 12,
+            spreadRadius: 8,
           ),
         ],
-        gradient: const LinearGradient(
-          colors: [
-            Color.fromARGB(255, 225, 254, 154),
-            Color.fromARGB(255, 165, 187, 110),
-            ColorsAppLight.primaryColor,
-            Color.fromARGB(255, 110, 125, 73),
-            Color.fromARGB(255, 97, 110, 65),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
+        // boxShadow: [
+        //   BoxShadow(
+        //     color: ColorsAppLight.primaryColor,
+        //     blurRadius: 25.sp,
+        //     spreadRadius: 15.sp,
+        //   ),
+        // ],
+        // gradient: const LinearGradient(
+        //   colors: [
+        //     Color.fromARGB(255, 162, 191, 89),
+        //     Color.fromARGB(255, 165, 187, 110),
+        //     ColorsAppLight.primaryColor,
+        //     Color.fromARGB(255, 110, 125, 73),
+        //     Color.fromARGB(255, 97, 110, 65),
+        //   ],
+        //   begin: Alignment.topLeft,
+        //   end: Alignment.bottomRight,
+        // ),
       ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Text(
             counter.toString(),
+            maxLines: 1,
             style: TextStyle(
-              fontSize: 60.sp,
+              fontSize: 100.sp,
               fontWeight: FontWeight.bold,
               color: Colors.white,
               fontFamily: "Amiri",
             ),
           ),
-          SizedBox(height: 10.h),
           Text(
-            'سبح',
+            selectedSabha,
+            textAlign: TextAlign.center,
             style: TextStyle(
-              fontSize: 20.sp,
+              fontSize: 40.sp,
               color: Colors.white,
               fontFamily: "Amiri",
             ),
